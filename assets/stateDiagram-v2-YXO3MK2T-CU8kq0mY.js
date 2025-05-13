43b2ebeb8ee683858b77622f70ab2cbaf85537b3
Roger Salas <rogerjs93@gmail.com>
2025-05-13 16:19:15 +0300

    Updates

--- assets/stateDiagram-v2-YXO3MK2T-CU8kq0mY.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as a,a as r,S as s}from"./chunk-AEK57VVT-B4JwidNb.js";import{_ as i}from"./index-B5shDuhe.js";import"./chunk-RZ5BOZE2-BgOvI2IM.js";var _={parser:r,get db(){return new s(2)},renderer:a,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{_ as diagram};
