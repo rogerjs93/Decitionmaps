f4285bc6ec18a147414649b46d49145921db8ba0
Roger Salas <rogerjs93@gmail.com>
2025-05-13 16:01:07 +0300

    Updates

---
+++ assets/classDiagram-GIVACNV2-D4LC8irZ.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-CB1F-rp1.js";import{_ as l}from"./index-B5shDuhe.js";import"./chunk-RZ5BOZE2-BgOvI2IM.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
